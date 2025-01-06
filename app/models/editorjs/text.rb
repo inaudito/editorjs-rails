1c65e9f315fee9de1cb5fe7fb3c5058bd95baa26
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2025-01-06 14:24:30 +0100

    Set coder

--- app/models/editorjs/text.rb
+++ app/models/editorjs/text.rb
@@ -6,7 +6,7 @@ class Editorjs::Text < Editorjs::ApplicationRecord
   belongs_to :record, polymorphic: true, touch: true
   has_many_attached :images
 
-  serialize :content, Editorjs::Text::Content
+  serialize :content, coder: Editorjs::Text::Content
   delegate :to_h, :as_json, :to_json, :to_html, :empty?, to: :content
 
   after_save :cleanup!, if: :saved_change_to_content?
